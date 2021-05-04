dba758107f42018f8b8cc5d5e005ae5774b010c5
Muhammad Rafif Ghani <80298487+RafifGhani@users.noreply.github.com>
2021-05-04 14:47:06 +0700

    Comment TopUp

--- WTFutball/app/Http/Controllers/TopupController.php
+++ WTFutball/app/Http/Controllers/TopupController.php
@@ -14,7 +14,14 @@ class TopupController extends Controller
     {
         return view('topup.index');
     }
-
+    /**
+    Fungsi ini akan melakukan pengecheckan terhadap database
+    apakah voucher sudah terdapat dalam database atau tidak.
+    jika voucher terdapat dalam database, maka saldo user akan ditambahkan
+    lalu akan mengeluarkan notif bahwa redeem voucer berhasil.
+    jika voucher tidak terdapat dalam database, maka akan mengeluarkan
+    notif bahwa voucher error.
+    */
     public function redeem(Request $request){
         $check = Topup::where('code', $request->voucherCode)->get();
 
@@ -29,13 +36,20 @@ class TopupController extends Controller
 
         return back()->with('voucher-404', 'voucher 404');
     }
-
-
+    /**
+    fungsi ini digunakan untuk menganti halaman
+    ke halaman dimana admin akan membuat code voucher
+    */
     public function create()
     {
         return view('topup.create');
     }
-
+    /**
+    fungsi ini digunakan untuk memasukkan voucher yang
+    telah dibuat oleh admin kedalam database.
+    code = code yang nantinya akan dimasukkan oleh user untuk topup
+    saldo = jumlah saldo yang akan ditambahkan kepada user ketika menggunakan code voucher
+    */
     public function store(Request $request)
     {
         Topup::insert([
