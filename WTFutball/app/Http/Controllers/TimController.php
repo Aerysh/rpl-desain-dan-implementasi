d3774be442ef0df89887b5333159730f3345ceaa
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2021-05-04 14:45:19 +0700

    Update TimController.php

--- WTFutball/app/Http/Controllers/TimController.php
+++ WTFutball/app/Http/Controllers/TimController.php
@@ -14,21 +14,24 @@ class TimController extends Controller
         return view('buatTim');
     }
 
+    /**
+    * Fungsi Buat Tim bagi User
+    */
     public function store(Request $request)
     {
         Tim::insert([
             'nama'      =>  $request->namaTim,
-        ]);
+        ]); // Insert nama tim ke database Tim
 
-        $timId  =   Tim::orderBy('id', 'desc')->select('id')->get();
+        $timId  =   Tim::orderBy('id', 'desc')->select('id')->get(); // mengambil id tim terbaru yang baru diinputkan ke database
 
         foreach($timId as $tim);
         DB::table('users_tim')->insert([
             'usersId'   =>  Auth::id(),
             'timId'     =>  $tim->id,
-        ]);
+        ]); // Insert id tim ke database users_tim
 
-        return redirect('home');
+        return redirect('home'); // redirect user ke home
     }
 
     public function show(Tim $tim)
